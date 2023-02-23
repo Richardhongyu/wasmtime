82da2dd86c854aec5aed828aad147a2eefa7d2a5
Li Hongyu <543306408@qq.com>
2023-02-23 05:43:28 -0800

    wasi-common: add the api of async-rdma example

--- crates/wasi-common/src/snapshots/preview_1.rs
+++ crates/wasi-common/src/snapshots/preview_1.rs
@@ -10,6 +10,13 @@ use crate::{
     },
     I32Exit, SystemTimeSpec, WasiCtx,
 };
+use async_rdma::{LocalMrReadAccess, LocalMrWriteAccess, RdmaBuilder, Rdma};
+use portpicker::pick_unused_port;
+use std::{
+    alloc::Layout,
+    io::{self, Write},
+    net::{Ipv4Addr, SocketAddrV4},
+};
 use cap_std::time::{Duration, SystemClock};
 use std::convert::{TryFrom, TryInto};
 use std::io::{IoSlice, IoSliceMut};
@@ -1261,9 +1268,65 @@ impl wasi_snapshot_preview1::WasiSnapshotPreview1 for WasiCtx {
 
     async fn rdma_list(&mut self) -> Result<(), Error> {
         println!("rdma_list test");
+        test();
         Ok(())
     }
+
+    // async fn rdma_builder(&mut self) -> Result<(), Error> {
+    //     println!("rdma_builder");
+    //     RdmaBuilder::default().connect(addr).await
+    // }
+
+    // async fn request_remote_mr(&mut self, rdma: Rdma, layout: Layout) -> Result<Rdma, Error> {
+    //     println!("request_remote_mr");
+    //     rdma.request_remote_mr(layout).await
+    // }
+
+    // async fn alloc_local_mr(&mut self, rdma: Rdma, layout: Layout) -> Result<RemoteMr, Error> {
+    //     println!("alloc_local_mr");
+    //     rdma.request_remote_mr(layout).await
+    // }
 }
+
+async fn client(addr: SocketAddrV4) -> io::Result<()> {
+    let layout = Layout::new::<[u8; 8]>();
+    let rdma = RdmaBuilder::default().connect(addr).await?;
+    // alloc 8 bytes remote memory
+    let mut rmr = rdma.request_remote_mr(layout).await?;
+    // alloc 8 bytes local memory
+    let mut lmr = rdma.alloc_local_mr(layout)?;
+    // write data into lmr
+    let _num = lmr.as_mut_slice().write(&[1_u8; 8])?;
+    // write the second half of the data in lmr to the rmr
+    rdma.write(&lmr.get(4..8).unwrap(), &mut rmr.get_mut(4..8).unwrap())
+        .await?;
+    // send rmr's meta data to the remote end
+    rdma.send_remote_mr(rmr).await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn server(addr: SocketAddrV4) -> io::Result<()> {
+    let rdma = RdmaBuilder::default().listen(addr).await?;
+    // receive mr's meta data from client
+    let lmr = rdma.receive_local_mr().await?;
+    let data = *lmr.as_slice();
+    println!("Data written by the client using RDMA WRITE: {:?}", data);
+    assert_eq!(data, [[0_u8; 4], [1_u8; 4]].concat());
+    Ok(())
+}
+
+#[tokio::main]
+async fn test() {
+    let addr = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), pick_unused_port().unwrap());
+    std::thread::spawn(move || server(addr));
+    tokio::time::sleep(Duration::new(1, 0)).await;
+    client(addr)
+        .await
+        .map_err(|err| println!("{}", err))
+        .unwrap();
+}
+
 
 impl From<types::Advice> for Advice {
     fn from(advice: types::Advice) -> Advice {
